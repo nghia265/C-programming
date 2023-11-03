bc7d46e4b4c91e7ada2e24775cf64a13e9ab35ff
nghia265 <149762294+nghia265@users.noreply.github.com>
2023-11-03 15:03:51 +0700

    Add files via upload

---
+++ Bai4.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("enter the value in feet:");scanf("%f",&a);
+    printf("the value converted from feet to meter=%f",b=a*0.304);
+    return 0;
+}
