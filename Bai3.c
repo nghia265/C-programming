21693b52c5c9830a0f9c04b41eb5634c4b7250cf
nghia265 <149762294+nghia265@users.noreply.github.com>
2023-11-03 14:52:46 +0700

    Add files via upload

---
+++ Bai3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float a, b, c;
+    printf("enter the first value:"); scanf("%f",&a);
+    printf("enter the other value:"); scanf("%f",&b);
+    printf("the result=%.2f",c=a*b);
+    return 0;
+}
