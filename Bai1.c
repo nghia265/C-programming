264568a863e8eab43e37375937b3b05cc1d9cc48
nghia265 <149762294+nghia265@users.noreply.github.com>
2023-11-03 14:43:06 +0700

    Add files via upload

---
+++ Bai1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("enter the first value:"); scanf("%d",&a);
+    printf("enter the other value:"); scanf("%d",&b);
+    printf("\nthe sum of a and b=%d",c=a+b);
+    return 0;
+}
