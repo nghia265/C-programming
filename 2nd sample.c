ad5e77a4888772947cb1f310a7e1b86e72bb0744
nghia265 <149762294+nghia265@users.noreply.github.com>
2023-11-03 14:09:47 +0700

    Add files via upload

---
+++ 2nd sample.c
@@ -0,0 +1,13 @@
+int main()
+{
+
+    int a = 10, b = 100;
+    float c = 10.5, d = 100.5;
+
+    printf("++a = %d\n",++a);
+    printf("--b = %d\n",--b);
+    printf("++c = %.1f\n",++c);
+    printf("--d = %.1f\n",--d);
+
+    return 0;
+}
